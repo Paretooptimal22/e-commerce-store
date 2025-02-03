1e3f1e6b4ae217de40a6bb775327111ea04e3074
kevin young <you.k2208@gmail.com>
2025-02-03 11:23:13 -0800

    added product model, route, controller, middleware - protectRoute, adminRoute

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -15,6 +16,7 @@ app.use(express.json()); // for parsing application/json
 app.use(cookieParser()); // for parsing cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log("Servier is running on http://localhost:" + PORT);
