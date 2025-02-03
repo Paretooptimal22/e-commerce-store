e42f04abfdf138fb3030277dcb4357b0d45477b8
kevin young <you.k2208@gmail.com>
2025-02-03 15:16:32 -0800

    added getFeaturedProducts route, controller, and createProduct route, controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -6,6 +7,59 @@ export const getAllProducts = async (req, res) => {
     res.json({ products });
   } catch (error) {
     console.log("Error in getAllProducts: ", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    // if not in redis cache, get from MongoDB
+    featuredProducts = await Product.find({ isFeatured: true }).lean(); //.lean() to get plain JS objects, good for performance
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "Featured products not found" });
+    }
+
+    // store in redis cache for quick access
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in getFeaturedProducts controller: ", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+
+    let cloudinaryResponse = null;
+
+    if (image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {
+        folder: "products",
+      });
+    }
+
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url
+        ? cloudinaryResponse.secure_url
+        : "",
+      category,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    console.log("Error in createProduct controller: ", error.message);
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
