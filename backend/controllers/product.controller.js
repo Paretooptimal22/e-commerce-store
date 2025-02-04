627cb0a070c9abad29136f155a9ea7fd2024d858
kevin young <you.k2208@gmail.com>
2025-02-04 09:49:09 -0800

    added toggleFeaturedProduct route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -124,3 +124,29 @@ export const getProductsByCategory = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
+
+export const toggleFeaturedProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      product.isFeatured = !product.isFeatured;
+      const updatedProduct = await product.save();
+      await updateFeaturedProductsCache();
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.log("Error in toggleFeaturedProduct controller: ", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+async function updateFeaturedProductsCache() {
+  try {
+    const featuredProducts = await Product.find({ isFeatured: true }).lean(); //.lean() to get plain JS objects, good for performance
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+  } catch (error) {
+    console.log("Error updating featured products cache: ", error.message);
+  }
+}
