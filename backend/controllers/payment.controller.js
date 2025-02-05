b7080bbeada88aa687fbb14504111bb3b24dbd81
kevin young <you.k2208@gmail.com>
2025-02-04 18:02:06 -0800

    added payment routes & controller, added stripe session code,  coupon model

---
+++ backend/controllers/payment.controller.js
@@ -0,0 +1,91 @@
+import { stripe } from "../lib/stripe.js";
+
+export const createCheckoutSession = async (req, res) => {
+  try {
+    const { products, couponCode } = req.body;
+
+    if (!Array.isArray(products) || products.length === 0) {
+      return res
+        .status(400)
+        .json({ message: "Invalid or empty products array" });
+    }
+
+    let totalAmount = 0;
+
+    const lineItems = products.map((product) => {
+      const amount = Math.round(product.price * 100); // Stripe requires amount in cents
+      totalAmount += amount * product.quantity;
+
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: product.title,
+            images: [product.image],
+          },
+          unit_amount: amount,
+        },
+      };
+    });
+
+    let coupon = null;
+    if (couponCode) {
+      coupon = await Coupon.findOne({
+        code: couponCode,
+        userId: req.user._id,
+        isActive: true,
+      });
+      if (coupon) {
+        totalAmount -= Math.round(
+          (totalAmount * coupon.discountPercentage) / 100
+        );
+      }
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${process.env.CLIENT_URL}payment-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.CLIENT_URL}/payment-cancel`,
+      discounts: coupon
+        ? [
+            {
+              coupon: await createStripeCoupon(coupon.discountPercentage),
+            },
+          ]
+        : [],
+      metadata: {
+        userId: req.user._id.toString(),
+        couponCode: couponCode || "",
+      },
+    });
+
+    if (totalAmount >= 20000) {
+      await createNewCoupon(req.user._id);
+    }
+    res.status(200).json({ is: session.id, totalAmount: totalAmount / 100 });
+  } catch (error) {}
+};
+
+async function createStripeCoupon(discountPercentage) {
+  const coupon = await stripe.coupons.create({
+    percent_off: discountPercentage,
+    duration: "once",
+  });
+
+  return coupon.id;
+}
+
+async function createNewCoupon(userId) {
+  const newCoupon = new Coupon({
+    code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+    userId: userId,
+  });
+
+  await newCoupon.save();
+
+  return newCoupon;
+}
